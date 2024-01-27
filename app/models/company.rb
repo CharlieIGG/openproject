6f9e1287dc5c81f9bacd4027df58f87cc500c050
Carlos Garcia <charlie.igg@gmail.com>
2024-01-26 23:56:45 -0600

    sets up missing models

---
+++ app/models/company.rb
@@ -0,0 +1,5 @@
+class Company < ApplicationRecord
+  belongs_to :owner, class_name: 'User'
+  has_many :parent_shares, class_name: 'Share', foreign_key: 'child_id', dependent: :destroy, inverse_of: :child
+  has_many :child_shares, class_name: 'Share', foreign_key: 'parent_id', dependent: :destroy, inverse_of: :parent
+end
