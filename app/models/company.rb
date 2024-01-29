7c59fe1ddbd241db453926569f10e940fa2e08ad
Carlos Garcia <charlie.igg@gmail.com>
2024-01-29 12:44:47 -0600

    implements top_owning_companies method for Company model, adjusts output for owning_users method

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,9 +1,34 @@
 class Company < ApplicationRecord
   belongs_to :owner, class_name: 'User'
-  has_many :parent_shares, class_name: 'Share', foreign_key: 'child_id', dependent: :destroy, inverse_of: :child
-  has_many :child_shares, class_name: 'Share', foreign_key: 'parent_id', dependent: :destroy, inverse_of: :parent
+  has_many :parent_shares, -> {
+                             where(active: true)
+                           }, class_name: 'Share', foreign_key: 'child_id', dependent: :destroy, inverse_of: :child
+  has_many :child_shares, -> {
+                            where(active: true)
+                          }, class_name: 'Share', foreign_key: 'parent_id', dependent: :destroy, inverse_of: :parent
 
   def owning_users
-    User.where(id: owner_id)
+    return User.where(id: owner_id) if parent_shares.empty?
+
+    User.where(id: top_owning_companies.pluck(:owner_id))
+  end
+
+  def top_owning_companies
+    top_companies = []
+    visited_companies = []
+    companies_stack = [self]
+
+    while companies_stack.length.positive?
+      current_company = companies_stack.pop
+      visited_companies << current_company
+
+      current_company.parent_shares.each do |share|
+        next if visited_companies.include?(share.parent)
+
+        companies_stack << share.parent
+      end
+    end
+
+    top_companies
   end
 end
